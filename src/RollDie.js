15e28aa88e4369d7992c49f0c99ca280a5c49df3
Naincy Mourya <naincycodes@gmail.com>
2022-07-06 18:33:42 +0530

    rolling dice complete

---
+++ src/RollDie.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Die from './Die'
+import './RollDie.css'
+
+class RollDie extends Component {
+  static defaultProps = {
+    sides: ['one', 'two', 'three', 'four', 'five', 'six'],
+  }
+  constructor(props) {
+    super(props)
+    this.state = { die1: 'one', die2: 'one', rolling: false }
+    this.roll = this.roll.bind(this)
+  }
+  roll() {
+    // pick 2 new rolls
+    const newDie1 =
+      this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
+    const newDie2 =
+      this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
+    // set state with new rolls
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true })
+    //wait one second and then set rolling to false
+    setTimeout(() => {
+        this.setState({rolling: false})
+    }, 1000)
+  }
+
+  render() {
+    return (
+      <div className='RollDie'>
+        <div className='rollDie-container'>
+          <Die face={this.state.die1} rolling={this.state.rolling} />
+          <Die face={this.state.die2} rolling={this.state.rolling} />
+        </div>
+        <button onClick={this.roll} disabled={this.state.rolling}>
+            {this.state.rolling ? "Rolling..." : "Roll Dice"}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default RollDie
